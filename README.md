629188fea67cf79c649048cc8d265915d49e059a
Chenglong <mestway@gmail.com>
2018-12-12 13:56:38 -0800

    minr

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 copy paste to make visualization design easier
 
+## Instructions
+
+Install dependencies: run `pip install -r requirements.txt`.
+
+Install falx in the development mode: `pip install -e .`, this is necessary to resolve dependencies.
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(name='falx',
       version='0.1',
       description='Visualization Replication',
-      author='Chenglong Wang, Yu Feng',
+      author='Chenglong Wang and Yu Feng',
       author_email='',
       url='https://github.com/Mestway/falx',
       packages=['falx'],
