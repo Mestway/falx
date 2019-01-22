2e41cdd7879875d4cd8b88a2c6e7c17867384f13
clwang <clwang@cs.washington.edu>
2019-01-22 10:56:46 -0800

    add js module

--- falx/run.py
+++ falx/run.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 import argparse
 import json
-from jsonschema
+import jsonschema
 import os
 from pprint import pprint
 
@@ -12,21 +12,27 @@ import design_enumerator
 import utils
 
 # default directories
-proj_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-temp_dir = os.path.join(proj_dir, "__temp__")
-if not os.path.exists(temp_dir): os.mkdir(temp_dir)
+PROJ_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+RESOURCE_DIR = os.path.join(PROJ_DIR, "resource")
+TEMP_DIR = os.path.join(PROJ_DIR, "__temp__")
+if not os.path.exists(TEMP_DIR): os.mkdir(TEMP_DIR)
 
 # arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_file", dest="input_file", default=None, 
 					help="input Vega-Lite spec file")
 parser.add_argument("--input_dir", dest="input_dir",
-					default=os.path.join(proj_dir, "vl_examples"),
+					default=os.path.join(PROJ_DIR, "vl_examples"),
 					help="input files; ignored if input-file is specified")
 parser.add_argument("--output_dir", dest="output_dir", 
-					default=temp_dir, help="output directory")
+					default=TEMP_DIR, help="output directory")
 
 def run(flags):
+	"""Synthesize vega-lite schema """
+
+	with open(os.path.join(RESOURCE_DIR, "vega-lite-schema.json")) as f:
+		vl_schema = json.load(f)
+
 	input_files = []
 	if flags.input_file is not None:
 		input_files.append(flags.input_file)
@@ -38,6 +44,7 @@ def run(flags):
 	vl_specs = [utils.load_vl_spec(f) for f in input_files]
 	data_urls = [spec["data"]["url"] for spec in vl_specs if "url" in spec["data"]]
 
+	print("# start enumeration")
 	output_index = 0
 	for vl_spec in vl_specs:
 		if "layer" in vl_spec or "transform" in vl_spec:
@@ -51,10 +58,19 @@ def run(flags):
 		candidates = design_enumerator.explore_designs(vl_spec, new_data, target_fields)
 		
 		for s in candidates:
-			print(json.dumps(s, indent=2))
+			try:
+				jsonschema.validate(s, vl_schema)
+				print(".", end="",flush=True)
+			except:
+				print("x", end="",flush=True)
+				pass
+			#print(json.dumps(s, indent=2))
 			with open(os.path.join(flags.output_dir, "temp_{}.vl.json".format(output_index)), "w") as g:
 				g.write(json.dumps(s))
 			output_index += 1
+	
+	print("")
+	print("# finish enumeration")
 
 if __name__ == '__main__':
 	flags = parser.parse_args()
