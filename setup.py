56b3939029f2d3551722788666fabecd7089ecdc
Chenglong Wang <mestway@gmail.com>
2019-03-22 00:13:27 -0700

    auto

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from setuptools import setup
 
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+
 setup(name='falx',
       version='0.1',
-      description='Visualization Replication',
+      description='Visualization By Example',
       author='Chenglong Wang and Yu Feng',
       author_email='',
       url='https://github.com/Mestway/falx',
       packages=['falx'],
-     )
+      cmdclass={'clean': CleanCommand,})
