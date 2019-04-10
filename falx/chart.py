126dcecbf37f1c88b9c31af7516e25a6f317ad12
Chenglong Wang <mestway@gmail.com>
2019-04-09 18:29:50 -0700

    add to_vl_json

--- falx/chart.py
+++ falx/chart.py
@@ -1,59 +1,126 @@
-import os
+from collections import namedtuple
+import json
+import copy
 
-class Visualization(object):
+
+Rect = namedtuple("Rect", ["x", "y", "color", "width", "height", "chart_id", "group"])
+Point = namedtuple("Point", ["shape", "x", "y", "size", "color", "chart_id", "group"])
+Line = namedtuple("Line", ["x", "y", "x_delta", "y_delta", "size", "chart_id", "group"])
+Area = namedtuple("Area", ["x", "y", "color", "chart_id", "group"])
+
+class VisDesign(object):
+	"""Top level visualization construct """
 	def __init__(self, data, chart):
 		self.data = data
 		self.chart = chart
 
-class LayeredChart(object):
-	def __init__(self, layers, shared_encs):
-		self.layers = layers
-		self.shared_encs = shared_encs
+	def to_vl_obj(self):
+		chart_obj = self.chart.to_vl_obj()
+		chart_obj["data"] = {"values": self.data}
+		return chart_obj
+
+	def to_vl_json(self):
+		return json.dumps(self.to_vl_obj())
+
+	def eval(self):
+		return self.chart.eval(self.data)
 
 class GroupChart(object):
+	"""Repeat the chart for multiple groups """
 	def __init__(self, enc_group, layer):
 		self.layer = layer
 		self.enc_group = enc_group
 
+	def to_vl_obj(self):
+		vl_obj = {
+			"mark": "bar",
+			"encoding": {e.channel:e.to_vl_obj() for e in self.encodings}
+		}
+		vl_obj["encoding"]["column"] = enc_group.to_vl_obj()
+		return vl_obj
+
+	def eval(self, data):
+		grouped_data = {}
+		group_vals = []
+		# TODO
+		pass
+
+
+class LayeredChart(object):
+	def __init__(self, layers, shared_encodings, resolve):
+		"""A layered bar chart where  """
+		self.layers = layers
+		self.shared_encodings = shared_encodings
+		self.resolve = resolve
+
+	def to_vl_obj(self):
+		layer_obj = [l.to_vl_obj() for l in self.layers]
+		for l in layer_obj:
+			l["mark"] = {"type": l["mark"], "opacity": 0.7}
+		vl_obj = {
+			"encoding": {e.channel:e.to_vl_obj() for e in self.shared_encodings},
+			"layer": layer_obj,
+			"resolve": self.resolve
+		}
+		return vl_obj
+
+	def eval(self, data):
+		result = []
+		for layer in self.layers:
+			inst_layer = copy.copy(layer)
+			inst_layer.encodings.extend(self.shared_encodings)
+			result += inst_layer.eval(data)
+		return result
+
 class BarChart(object):
-	def __init__(self, enc_x, enc_y, enc_x2, enc_y2):
-		self.enc_x = enc_x
-		self.enc_y = enc_y
-		self.enc_x2 = enc_x2
-		self.enc_y2 = enc_y2
+	def __init__(self, encodings):
+		"""encodings of x,y,x2,y2"""
+		self.encodings = encodings
+
+	def to_vl_obj(self):
+		return {
+			"mark": "bar",
+			"encoding": {e.channel:e.to_vl_obj() for e in self.encodings}
+		}
 
 class StackedChart(object):
-	def __init__(self, chart_ty, stack_ty, enc_x, enc_y, enc_x2, enc_y2, enc_color):
+	def __init__(self, chart_ty, stack_channel, stack_ty, encodings):
+		""" encodings x,y,x2,y2,color"""
 		self.chart_ty = chart_ty
-		self.enc_x = enc_x
-		self.enc_y = enc_y
-		self.enc_x2 = enc_x2
-		self.enc_y2 = enc_y2
-		self.enc_color = enc_color
+		self.stack_channel = stack_channel
 		self.stack_ty = stack_ty
+		self.encodings = encodings
+
+	def to_vl_obj(self):
+		vl_obj = {
+			"mark": self.chart_ty,
+			"encoding": {e.channel:e.to_vl_obj() for e in self.encodings}
+		}
+		vl_obj["encoding"][self.stack_channel]["stack"] = self.stack_ty
+		return vl_obj
 
 class LineChart(object):
-	def __init__(self, enc_x, enc_y, enc_color=None, enc_size=None):
-		self.enc_x = enc_x
-		self.enc_y = enc_y
-		self.enc_color = enc_color
-		self.enc_size = enc_size
+	def __init__(self, encodings):
+		"""Encodings x,y,color,size"""
+		self.encodings = encodings
 
-	def to_vl_json(self):
-		encodings = {}
+	def to_vl_obj(self):
 		return {
 			"mark": "line",
-			"encoding": build_encs_vl_json([self.enc_x, self.enc_y, self.enc_color, self.enc_size])
+			"encoding": {e.channel:e.to_vl_obj() for e in self.encodings}
 		}
 
 class ScatterPlot(object):
-	def __init__(self, mark_ty, enc_x, enc_y, enc_color, enc_size, enc_shape):
+	def __init__(self, mark_ty, encodings):
+		"""enc_x, enc_y, enc_color, enc_size, enc_shape"""
 		self.mark_ty = mark_ty
-		self.enc_x = enc_x
-		self.enc_y = enc_y
-		self.enc_color = enc_color
-		self.enc_size = enc_size
-		self.enc_shape = enc_shape
+		self.encodings = encodings
+
+	def to_vl_obj(self):
+		return {
+			"mark": self.mark_ty,
+			"encoding": {e.channel:e.to_vl_obj() for e in self.encodings}
+		}
 
 class Encoding(object):
 	def __init__(self, channel, field, enc_ty, sort_ty=None):
@@ -62,15 +129,8 @@ class Encoding(object):
 		self.enc_ty = enc_ty
 		self.sort_ty = sort_ty
 
-	def to_vl_json(self):
-		res = {"channel": self.channel, "field": self.field, "type": self.enc_ty}
+	def to_vl_obj(self):
+		res = {"field": self.field, "type": self.enc_ty}
 		if self.sort_ty:
 			res["sort_ty"] = self.sort_ty
 		return res
-
-def build_encs_vl_json(encodings):
-	res = {}
-	for e in encodings:
-		if e is not None:
-			res[e.channel] = e.to_vl_json()
-	return res
--- test/test_chart.py
+++ test/test_chart.py
@@ -1,14 +1,79 @@
 import unittest
-from falx.chart import LineChart, Encoding
+from falx.chart import *
+
+example_data = [{"Totals":7,"Value":"A","variable":"alpha","value":2,"cumsum":2},
+                {"Totals":8,"Value":"B","variable":"alpha","value":2,"cumsum":2},
+                {"Totals":9,"Value":"c","variable":"alpha","value":3,"cumsum":3},
+                {"Totals":9,"Value":"D","variable":"alpha","value":3,"cumsum":3},
+                {"Totals":9,"Value":"E","variable":"alpha","value":4,"cumsum":4},
+                {"Totals":7,"Value":"A","variable":"beta","value":2,"cumsum":4},
+                {"Totals":8,"Value":"B","variable":"beta","value":3,"cumsum":5},
+                {"Totals":9,"Value":"c","variable":"beta","value":3,"cumsum":6},
+                {"Totals":9,"Value":"D","variable":"beta","value":4,"cumsum":7},
+                {"Totals":9,"Value":"E","variable":"beta","value":3,"cumsum":7},
+                {"Totals":7,"Value":"A","variable":"gamma","value":3,"cumsum":7},
+                {"Totals":8,"Value":"B","variable":"gamma","value":3,"cumsum":8},
+                {"Totals":9,"Value":"c","variable":"gamma","value":3,"cumsum":9},
+                {"Totals":9,"Value":"D","variable":"gamma","value":2,"cumsum":9},
+                {"Totals":9,"Value":"E","variable":"gamma","value":2,"cumsum":9}]
 
 class TestChart(unittest.TestCase):
 
     def test_gen_chart(self):
-        line_chart = LineChart(
-            enc_x = Encoding("x", "c1", "nominal"),
-            enc_y = Encoding("y", "c2", "quantitative"))
 
-        print(line_chart.to_vl_json())
+        chart = LineChart(
+            encodings=[ Encoding("x", "Value", "nominal"),
+                        Encoding("y", "Totals", "quantitative") ])
+
+        print()
+        print(VisDesign(chart=chart, data=example_data).to_vl_json())
+        print()
+
+    def test_stacked(self):
+
+        chart = StackedChart(
+            chart_ty="area",
+            stack_channel="y",
+            stack_ty="normalize",
+            encodings=[ Encoding("x", "Value", "nominal"),
+                        Encoding("y", "value", "quantitative"),
+                        Encoding("color", "variable", "nominal") ])
+        print()
+        print(VisDesign(chart=chart, data=example_data).to_vl_json())
+        print()
+
+    def test_scatter(self):
+
+        chart = ScatterPlot(
+            mark_ty="point",
+            encodings=[ Encoding("x", "Value", "nominal"),
+                        Encoding("size", "value", "quantitative"),
+                        Encoding("y", "variable", "nominal") ])
+        print()
+        print(VisDesign(chart=chart, data=example_data).to_vl_json())
+        print()
+
+    def test_layered_scatter(self):
+
+        line_chart = LineChart(
+            encodings=[ Encoding("y", "Totals", "quantitative") ])
+
+        area_chart = StackedChart(
+            chart_ty="area",
+            stack_channel="y",
+            stack_ty="normalize",
+            encodings=[ Encoding("y", "value", "quantitative"),
+                        Encoding("color", "variable", "nominal") ])
+
+        chart = LayeredChart(
+            shared_encodings=[Encoding("x", "Value", "nominal")],
+            layers=[line_chart, area_chart],
+            resolve={"scale": {"y": "independent"}})
+
+        print()
+        print(VisDesign(chart=chart, data=example_data).to_vl_json())
+        print()
+
 
 if __name__ == '__main__':
     unittest.main()
