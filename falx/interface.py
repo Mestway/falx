ea8dfaac984dbc0f44df72c2905fdc6c0e5b50ad
Chenglong Wang <mestway@gmail.com>
2019-04-30 15:57:21 -0700

    update interface

--- falx/interface.py
+++ falx/interface.py
@@ -18,30 +18,33 @@ class FalxTask(object):
         abstract_designs = VisDesign.inv_eval(self.vtrace)
 
         for sym_data, chart in abstract_designs:
+            if not isinstance(sym_data, (list,)):
+                # single-layer chart
+                candidate_progs = morpheus.synthesize(self.inputs, sym_data)
+                for p in candidate_progs:
+                    vis_design = VisDesign(data=morpheus.evalute(p, self.inputs), chart=chart)
+                    candidates.append((p, vis_design))
+            else: 
+                # multi-layer charts
+                # layer_candidate_progs[i] contains all programs that transform inputs to output[i]
 
-            # there could be multiple output tables for multi-layered charts
-            # candidates_per_layer[i] contains all programs that transforms inputs to output[i]
+                layer_candidate_progs = []
+                for output in sym_data:
+                    # synthesize table transformation programs
+                    layer_candidate_progs.append(morpheus.synthesize(self.inputs, output))
 
-            candidates_per_layer = []
+                layer_id_lists = [list(range(len(l))) for l in layer_candidate_progs]
+                
+                # iterating over combinations for different layers
+                for layer_id_choices in itertools.product(*layer_id_lists):
 
-            sym_tables = sym_data if isinstance(sym_data, (list,)) else [sym_data]
-            for output in sym_tables:
-                # synthesize table transformation programs
-                candidates_per_layer.append(morpheus.synthesize(self.inputs, output))
+                    #layer_prog[i] is the transformation program for the i-th layer
+                    progs = [candidates[i][layer_id_choices[i]] for i in range(len(layer_id_choices))]
 
-            num_candidates_per_layer = [list(range(len(l))) for l in candidates_per_layer]
-            
-            # iterating over combinations for different layers
-            for id_list in itertools.product(*num_candidates_per_layer):
-                progs_per_layer = [candidates[i][id_list[i]] for i in range(len(id_list))]
+                    # apply each program on inputs to get output table for each layer
+                    outputs = [morpheus.evalute(p, self.inputs) for p in progs]
 
-                # apply each program on inputs to get output table for each layer
-                outputs = [morpheus.evalute(p, self.inputs) for p in progs_per_layer]
-
-                data = outputs[0] if len(outputs) == 1 else outputs
-                prog = progs_per_layer[0] if len(progs_per_layer) == 1 else progs_per_layer
-
-                vis_design = VisDesign(data=data, chart=chart)
-                candidates.append((prog, vis_design))
+                    vis_design = VisDesign(data=outputs, chart=chart)
+                    candidates.append((progs, vis_design))
 
         return candidates
