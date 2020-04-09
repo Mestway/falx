127577658679dfab5ad291d7ffbd90815b58de3d
Chenglong Wang <mestway@gmail.com>
2020-04-09 10:29:03 -0700

    update port

--- server/run_in_docker.sh
+++
@@ -1,3 +0,0 @@
-echo "# Start Server"
-python -m flask run --host=0.0.0.0
-echo "# Done"
--- server/server.py
+++ server/server.py
@@ -178,5 +178,5 @@ def run_falx_synthesizer():
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
-    #app.run(host='0.0.0.0', port=5000)
+    #app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(host='0.0.0.0', port=8590)
