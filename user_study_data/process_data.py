1627aa41d6f7c9bf8b3ecacebc646276b63e1c0f
Chenglong Wang <mestway@gmail.com>
2020-05-22 11:12:54 -0700

    add car sales data

--- user_study_data/process_data.py
+++ user_study_data/process_data.py
@@ -10,16 +10,16 @@ import json
 # 	#print(json.dumps(data))
 
 
-with open("raw/disasters.json", "r") as f:
-	data = json.load(f)
-	for r in data:
-		for key, val in r.items():
-			if val == None:
-				r[key] = 0
-		for key in ["Extreme weather", "Landslide", "Mass movement (dry)", "Volcanic activity", "Wildfire", "All natural disasters"]:
-			del r[key]
-	data = json.dumps([r for r in data if r["Year"] > 1960])
-	print(data)
+# with open("raw/disasters.json", "r") as f:
+# 	data = json.load(f)
+# 	for r in data:
+# 		for key, val in r.items():
+# 			if val == None:
+# 				r[key] = 0
+# 		for key in ["Extreme weather", "Landslide", "Mass movement (dry)", "Volcanic activity", "Wildfire", "All natural disasters"]:
+# 			del r[key]
+# 	data = json.dumps([r for r in data if r["Year"] > 1960])
+# 	print(data)
 
 # with open("raw/pge-electric-data.json", "r") as f:
 # 	data = json.load(f)
@@ -52,26 +52,28 @@ with open("raw/disasters.json", "r") as f:
 
 # from collections import OrderedDict
 
-# with open("raw/car-fuel-efficiency.json", "r") as f:
-# 	data = json.load(f)
-# 	for r in data:
-# 		del r["index"]
-# 		del r["efficiency"]
+with open("raw/car-sales.json", "r") as f:
+	data = json.load(f)
+	for r in data:
+		del r["index"]
+		del r["efficiency"]
+		for key in ['Passenger Car (Domestic)', 'Passenger Car (Imported)', 'Light truck']:
+			del r[key]
 
-# 	#data = json.dumps(data)
+	#data = json.dumps(data)
 
-# 	sort_order = ['year', 'Passenger Car (Domestic)', 'Passenger Car (Imported)', 'Light truck', "sales"]
-# 	data = [OrderedDict(sorted(item.items(), key=lambda k: sort_order.index(k[0]))) for item in data]
+	#sort_order = ['year', 'Passenger Car (Domestic)', 'Passenger Car (Imported)', 'Light truck', "sales"]
+	#data = [OrderedDict(sorted(item.items(), key=lambda k: sort_order.index(k[0]))) for item in data]
 
 	
 
-# 	last_sale = None
-# 	for r in data:
-# 		temp = r["sales"]
-# 		if last_sale != None:
-# 			r["sales"] = r["sales"] - last_sale
-# 		last_sale = temp
+	last_sale = None
+	for r in data:
+		temp = r["sales"]
+		if last_sale != None:
+			r["sales"] = r["sales"] - last_sale
+		last_sale = temp
 
-# 	data = json.dumps(data, indent=4, separators=(',', ': '))
+	data = json.dumps(data, indent=4, separators=(',', ': '))
 
-	# print(data)	
+	print(data)	
